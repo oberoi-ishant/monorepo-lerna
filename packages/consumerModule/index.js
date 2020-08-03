8d559e1ca12acbdb4ee1b79f017743ee1fa4440b
ishant.oberoi <ishant.oberoi@nutanix.com>
2020-08-03 16:50:54 +0530

    first commit

---
+++ packages/consumerModule/index.js
@@ -0,0 +1,8 @@
+// https://www.youtube.com/watch?v=Nn8G91x8tJI
+var add = require('additionModule');
+var substract = require('substractionModule');
+
+console.log('------------Consumer Module------------');
+console.log(`Addition a+b: ${add(4,5)}`);
+console.log(`Substraction a-b: ${substract(5,4)}`);
+console.log('---------------------------------------');
